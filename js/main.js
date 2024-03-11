336db0c2b06469afa89de68d390650afca1e921e
MMik1 <m.mik@sintlucasedu.nl>
2024-03-11 15:43:24 +0100

    Searchbar | Header working

--- js/main.js
+++ js/main.js
@@ -1,28 +1,53 @@
 document.addEventListener('DOMContentLoaded', function () {
     const searchIcon = document.querySelector('.search-icon');
     const searchContainer = document.querySelector('.search');
-    const searchForm = document.getElementById('searchForm');
     const searchInput = document.getElementById('searchInput');
+    const searchForm = document.getElementById('searchForm');
 
     searchIcon.addEventListener('click', function () {
         searchContainer.classList.toggle('active');
+        const isActive = searchContainer.classList.contains('active');
 
-        if (searchContainer.classList.contains('active')) {
-            searchInput.style.width = '300px'; // Adjust the width as needed
+        if (isActive) {
+            searchInput.style.width = '300px';
+            searchInput.style.opacity = '1';
+            searchInput.style.position = 'relative';
+            searchInput.style.zIndex = '1';
             searchInput.focus();
         } else {
             searchInput.style.width = '0';
+            searchInput.style.opacity = '0';
+            searchInput.style.position = 'absolute';
+            searchInput.style.zIndex = '-1';
         }
     });
 
     searchForm.addEventListener('submit', function (event) {
         event.preventDefault();
-
         const searchTerm = searchInput.value.toLowerCase();
 
-        // Your logic for handling the search term...
-
-        // For example, you can add a console log for testing:
-        console.log('Search term:', searchTerm);
+       
+        switch (searchTerm) {
+            case 'nieuws':
+                window.location.href = 'pages/nieuws.html';
+                break;
+          
+            case 'main':
+                window.location.href = 'pages/main.html';
+                break;
+            case 'shop':
+                window.location.href = 'pages/shop.html';
+                break;
+            case 'contacten':
+                window.location.href = 'pages/contacten.html';
+                break;
+            case 'credits':
+                window.location.href = 'pages/credits.html';
+                break;
+            default:
+                
+                alert('Zoek opdracht niet gevonden.');
+                break;
+        }
     });
 });
